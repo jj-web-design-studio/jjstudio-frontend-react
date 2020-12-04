a70ff85b07d1e5e6ed9f9393cf58514795b367ff
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-03 22:51:27 -0800

    Make individual note draggable and fixed left positions

--- src/components/soundbar/note.jsx
+++ src/components/soundbar/note.jsx
@@ -1,24 +1,55 @@
+import React, { useState, useRef } from "react";
+
 const Note = (props) => {
+  const [ isDragging, setDragging ] = useState(false);
+  const [ left, setLeft ] = useState(props.left);
 
-  const handleOnDrag = (e) => {
-    
-  }
+  const noteReference = useRef();
 
-  const handleOnDragEnd = (e) => {
+  const handleMouseDown = (e) => {
+    if (e.button !== 0) return;
+    setDragging(true);
+    setLeft(e.pageX / props.windowWidth * 100);
+ 
+    e.stopPropagation();
+    e.preventDefault();
+  };
 
+  const handleMouseUp = (e) => {
+    setDragging(false);
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    setLeft(e.pageX / props.windowWidth * 100);
+  
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const roundToNearestMultiple = (left) => {
+    console.log("left/0.78125 = " + left/0.78125)
+    console.log("Math.floor(left/0.7812) = " + Math.floor(left/0.7812))
+    console.log("Math.floor(left/0.78125) * 0.78125 = " + Math.floor(left/0.78125) * 0.78125)
+    let rounded = (Math.ceil(left/0.78125) * 0.78125) 
+    return rounded
   }
 
   return (
     <div
+      ref={noteReference}
       style={{
-        width: 10,
+        width: props.windowWidth/128,
         height: 40,
-        left: props.left,
+        left: roundToNearestMultiple(left) + "%",
         backgroundColor: "blue",
         position: "absolute",
       }}
-      onDrag={handleOnDrag}
-      onDragEnd={handleOnDragEnd}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
     />
   );
 };
