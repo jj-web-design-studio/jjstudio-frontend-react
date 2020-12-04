889a61432ba1a58178e6d27df60d5a9b24d3e00f
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-03 22:46:13 -0800

    Hook up keyboard dropdown with backend API

--- src/components/keyboard/keyboard.jsx
+++ src/components/keyboard/keyboard.jsx
@@ -1,49 +1,98 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
 import KeyboardDropdown from "./keyboard_dropdown";
-import Key from './key';
+import Key from "./key";
 import { NUM_ROW, QWE_ROW, ASD_ROW, ZXC_ROW } from "./keys";
-import './keyboard.css';
+import "./keyboard.css";
 
 const Keyboard = (props) => {
-  const isAuthenticated  = useSelector(state => state.session.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.session.isAuthenticated);
 
-  const { loadDefaultKeyboard, loadKeyboardNameList, keyboardName, numRowMapping, qweRowMapping, asdRowMapping, zxcRowMapping } = props;
+  const { loadDefaultKeyboard, loadKeyboardNameList } = props;
+
+  const { mapping } = props;
 
   useEffect(() => {
-    
     loadDefaultKeyboard();
-
   }, [loadDefaultKeyboard]);
 
   return (
-    <div className="keyBoard">
-      <div className="keyRow">
-        { isAuthenticated ? <KeyboardDropdown current={keyboardName} loadKeyboardNameList={loadKeyboardNameList} /> : <></>}
-      </div>
-      <div className="numRow keyRow">
-        {NUM_ROW.map((key) => (
-          <Key key={key.keyCode} keyCode={key.keyCode} label={key.label} soundId={numRowMapping != null ? numRowMapping[key.keyCode] : ""} />
-        ))}
-      </div>
-      <div className="qweRow keyRow">
-        {QWE_ROW.map((key) => (
-          <Key key={key.keyCode} keyCode={key.keyCode} label={key.label} soundId={qweRowMapping != null ? qweRowMapping[key.keyCode] : ""} />
-        ))}
-      </div>
-      <div className="asdRow keyRow">
-        {ASD_ROW.map((key) => (
-          <Key key={key.keyCode} keyCode={key.keyCode} label={key.label} soundId={asdRowMapping != null ? asdRowMapping[key.keyCode] : ""} />
-        ))}
-      </div>
-      <div className="zxcRow keyRow">
-        {ZXC_ROW.map((key) => (
-          <Key key={key.keyCode} keyCode={key.keyCode} label={key.label} soundId={zxcRowMapping != null ? zxcRowMapping[key.keyCode] : ""} />
-        ))}
-      </div>
+    <div>
+      {mapping != null ? (
+        <div className="keyBoard">
+          <div className="keyRow">
+            {isAuthenticated ? (
+              <KeyboardDropdown
+                current={mapping.name}
+                loadKeyboardNameList={loadKeyboardNameList}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="numRow keyRow">
+            {NUM_ROW.map((key) => (
+              <Key
+                key={key.keyCode}
+                keyCode={key.keyCode}
+                label={key.label}
+                soundId={
+                  mapping.numRowMapping != null
+                    ? mapping.numRowMapping[key.keyCode]
+                    : ""
+                }
+              />
+            ))}
+          </div>
+          <div className="qweRow keyRow">
+            {QWE_ROW.map((key) => (
+              <Key
+                key={key.keyCode}
+                keyCode={key.keyCode}
+                label={key.label}
+                soundId={
+                  mapping.qweRowMapping != null
+                    ? mapping.qweRowMapping[key.keyCode]
+                    : ""
+                }
+              />
+            ))}
+          </div>
+          <div className="asdRow keyRow">
+            {ASD_ROW.map((key) => (
+              <Key
+                key={key.keyCode}
+                keyCode={key.keyCode}
+                label={key.label}
+                soundId={
+                  mapping.asdRowMapping != null
+                    ? mapping.asdRowMapping[key.keyCode]
+                    : ""
+                }
+              />
+            ))}
+          </div>
+          <div className="zxcRow keyRow">
+            {ZXC_ROW.map((key) => (
+              <Key
+                key={key.keyCode}
+                keyCode={key.keyCode}
+                label={key.label}
+                soundId={
+                  mapping.zxcRowMapping != null
+                    ? mapping.zxcRowMapping[key.keyCode]
+                    : ""
+                }
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default withRouter(Keyboard);
