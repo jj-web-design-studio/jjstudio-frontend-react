a47f9dae4f5678f86e6bb6a73c7c92e377131568
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-05 17:03:09 -0800

    Remove getDefaultKeyboard API call

--- src/util/keyboard_api_util.js
+++ src/util/keyboard_api_util.js
@@ -4,10 +4,6 @@ export const getKeyboardById = (id) => {
   return axios.get("/v1/me/keyboards/" + id);
 }
 
-// export const getDefaultKeyboard = () => {
-//   return axios.get("/v1/me/keyboards/default");
-// }
-
 export const getKeyboardNameList = () => {
   return axios.get("/v1/me/keyboards");
 }
