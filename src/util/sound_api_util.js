26b987b0b154ec2db247243ba95f0dd5d588118e
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-06 23:58:58 -0800

    Fix SoundAPIUtil to use query param ids

--- src/util/sound_api_util.js
+++ src/util/sound_api_util.js
@@ -5,9 +5,15 @@ export const getSoundById = (id) => {
 };
 
 export const getSoundByIds = (ids) => {
-  debugger
-  const params = {
-    "ids": ids
+  let idQueryString = "";
+  for (let i = 0; i < ids.length; i++) {
+    idQueryString += ids[i]
+    if (i !== ids.length - 1) {
+      idQueryString += ",";
+    }
   }
-  return axios.get("/v1/me/sounds?ids=5fcadb14d62a7b2f68b3cb27,5fcadf1dd62a7b2f68b3cb28");
+  const params = {
+    ids: idQueryString
+  }
+  return axios.get("/v1/me/sounds", { params } );
 };
