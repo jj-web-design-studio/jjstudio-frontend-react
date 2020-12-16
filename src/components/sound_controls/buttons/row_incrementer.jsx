07e7514041d9819416b7e3efca60ad83532597ae
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-15 19:44:58 -0800

    Add styling for cursors and hover opacity to sound control buttons

--- src/components/sound_controls/buttons/row_incrementer.jsx
+++ src/components/sound_controls/buttons/row_incrementer.jsx
@@ -6,6 +6,8 @@ import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 
 const RowIncrementer = (props) => {
   const [rows, setRows] = useState(1);
+  const [isHoveringMinus, setHoveringMinus] = useState(false);
+  const [isHoveringPlus, setHoveringPlus] = useState(false);
 
   const useStyles = makeStyles({
     removeCircleStyle: {
@@ -16,22 +18,26 @@ const RowIncrementer = (props) => {
 
   return (
     <div className="soundBtn">
-      <div className="soundBtn">
+      <div className={isHoveringMinus ? "soundBtn hover" : "soundBtn"}>
         <RemoveCircleIcon
           className={classes.removeCircleStyle}
           onClick={() => {
             rows > 1 ? setRows(rows - 1) : setRows(1);
             rows > 1 ? props.setRows(rows - 1) : props.setRows(1);
           }}
+          onMouseEnter={() => {setHoveringMinus(true)}}
+          onMouseLeave={() => {setHoveringMinus(false)}}
         />
       </div>
       <div className="soundBtn">Rows: {rows}</div>
-      <div className="soundBtn">
+      <div className={isHoveringPlus ? "soundBtn hover" : "soundBtn"}>
         <AddCircleIcon
           onClick={() => {
             setRows(rows + 1);
             props.setRows(rows + 1);
           }}
+          onMouseEnter={() => {setHoveringPlus(true)}}
+          onMouseLeave={() => {setHoveringPlus(false)}}
         />
       </div>
     </div>
