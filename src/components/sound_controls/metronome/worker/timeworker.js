381e4a6ab52b1fb2a597a899d71f789fdc443862
Justin Chung <justin.chung.zsl@gmail.com>
2020-12-15 17:47:12 -0800

    Remove log statement from worker

--- src/components/sound_controls/metronome/worker/timeworker.js
+++ src/components/sound_controls/metronome/worker/timeworker.js
@@ -3,14 +3,11 @@ var interval = 100;
 
 self.onmessage = function (e) {
   if (e.data == "start") {
-    console.log("starting");
     timerID = setInterval(function () {
       postMessage("tick");
     }, interval);
   } else if (e.data.interval) {
-    console.log("setting interval");
     interval = e.data.interval;
-    console.log("interval=" + interval);
     if (timerID) {
       clearInterval(timerID);
       timerID = setInterval(function () {
@@ -18,10 +15,9 @@ self.onmessage = function (e) {
       }, interval);
     }
   } else if (e.data == "stop") {
-    console.log("stopping");
     clearInterval(timerID);
     timerID = null;
   }
 };
 
-postMessage("hi there");
+postMessage("Handshake acknowledged");
