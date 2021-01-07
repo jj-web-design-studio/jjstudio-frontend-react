7114740b8d38b8c4f35e5d7293ccfb67e91635fa
Justin Chung <justin.chung.zsl@gmail.com>
2021-01-06 18:40:45 -0800

    Simplify logic in index.js

--- src/index.js
+++ src/index.js
@@ -6,54 +6,37 @@ import jwt_decode from "jwt-decode";
 import { setAuthToken } from "./components/session/sessionAPIUtil";
 import { logout } from "./components/session/sessionActions";
 
+const preloadedState = {
+  keyboard: { currentKeyboardId: "default" },
+  track: {
+    track: {
+      bpm: 125,
+      timeSignature: {
+        beat: 4,
+        measure: 4,
+      },
+      rowCount: 1,
+      contents: [[]],
+    },
+  },
+  ui: {
+    proTip: { shouldPromptProTipKeyboard: true },
+  },
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (localStorage.jwtToken) {
     setAuthToken(localStorage.jwtToken);
     const decodedUser = jwt_decode(localStorage.jwtToken);
-    const preloadedState = {
-      session: { isAuthenticated: true, user: decodedUser },
-      keyboard: { currentKeyboardId: "default" },
-      track: {
-        track: {
-          bpm: 125,
-          timeSignature: {
-            beat: 4,
-            measure: 4,
-          },
-          rowCount: 1,
-          contents: [[]],
-        },
-      },
-      ui: {
-        proTip: { shouldPromptProTipKeyboard: true },
-      },
-    };
-    store = configureStore(preloadedState);
+    preloadedState.session = { isAuthenticated: true, user: decodedUser };
     const currentTime = Date.now() / 1000;
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
       window.location.href = "/";
     }
-  } else {
-    store = configureStore({
-      keyboard: { currentKeyboardId: "default" },
-      track: {
-        track: {
-          bpm: 125,
-          timeSignature: {
-            beat: 4,
-            measure: 4,
-          },
-          rowCount: 1,
-          contents: [[]],
-        },
-      },
-      ui: {
-        proTip: { shouldPromptProTipKeyboard: true },
-      },
-    });
   }
+  store = configureStore(preloadedState);
 
   const root = document.getElementById("root");
 
