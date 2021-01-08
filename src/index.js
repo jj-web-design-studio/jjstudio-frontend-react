a58014b6c5538f2f7118bb00a6e869b15bc2a6c6
Justin Chung <justin.chung.zsl@gmail.com>
2021-01-08 11:33:44 -0800

    Fix store in index.js

--- src/index.js
+++ src/index.js
@@ -24,19 +24,24 @@ const preloadedState = {
   },
 };
 
+function isUserSessionExpired(expirationTime, currentTime) {
+  return expirationTime < currentTime;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (localStorage.jwtToken) {
     setAuthToken(localStorage.jwtToken);
     const decodedUser = jwt_decode(localStorage.jwtToken);
     preloadedState.session = { isAuthenticated: true, user: decodedUser };
-    const currentTime = Date.now() / 1000;
-    if (decodedUser.exp < currentTime) {
+    store = configureStore(preloadedState);
+    if (isUserSessionExpired(decodedUser.exp, Date.now() / 1000)) {
       store.dispatch(logout());
       window.location.href = "/";
     }
+  } else {
+    store = configureStore(preloadedState);
   }
-  store = configureStore(preloadedState);
 
   const root = document.getElementById("root");
 
