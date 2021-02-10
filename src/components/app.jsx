694635c5532877d76b9e6bfd7ad484be97a55ef4
Justin Chung <justin.chung.zsl@gmail.com>
2021-02-09 23:00:05 -0800

    Reformat app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
+import React from "react";
 import Modal from "./common/modal/modal";
-import { Switch } from 'react-router-dom';
-import AppNavBar from "./navbar/navbar"
+import { Switch } from "react-router-dom";
+import AppNavBar from "./navbar/navbar";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import KeyboardContainer from './keyboard/keyboardContainer';
-import SoundControls from './soundControls/soundControls';
+import "bootstrap/dist/css/bootstrap.min.css";
+import KeyboardContainer from "./keyboard/keyboardContainer";
+import SoundControls from "./soundControls/soundControls";
 
 import "../styles/app.scss";
 
 const App = () => {
-    return (
-        <> 
-            <div>
-                <Modal />
-                <AppNavBar />
-                <KeyboardContainer />
-                <SoundControls />
-                <Switch>
-                    {/* <Route exact path = "/" component={HomeContainer} /> */}
-                </Switch>
-            </div>
-        </>
-    )
-}
+  return (
+    <div className="root">
+      <Modal />
+      <AppNavBar />
+      <KeyboardContainer />
+      <SoundControls />
+      <Switch>
+        {/* <Route exact path = "/" component={HomeContainer} /> */}
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
 
