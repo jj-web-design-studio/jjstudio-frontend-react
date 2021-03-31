f03434a76c0512608458bed22681d3936fe43b34
Justin Chung <justin.chung.zsl@gmail.com>
2021-03-30 23:17:07 -0700

    Update URLs for sound API

--- src/util/soundAPIUtil.js
+++ src/util/soundAPIUtil.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export const getSoundById = (id) => {
-  return axios.get("/v1/me/sounds/" + id);
+  return axios.get("/v1/sounds/" + id);
 };
 
 export const getSoundsByIds = (ids) => {
   let idQueryString = "";
   for (let i = 0; i < ids.length; i++) {
-    idQueryString += ids[i]
+    idQueryString += ids[i];
     if (i !== ids.length - 1) {
       idQueryString += ",";
     }
   }
   const params = {
-    ids: idQueryString
-  }
-  return axios.get("/v1/me/sounds", { params } );
+    ids: idQueryString,
+  };
+  return axios.get("/v1/sounds", { params });
 };
